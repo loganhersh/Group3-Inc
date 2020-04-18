1e969a5ba5ba6b4bc2c978a3b92991920ba35c65
loganhersh <47787402+loganhersh@users.noreply.github.com>
2020-04-18 14:41:17 -0400

    added users

--- backend/sql/create-users-table.sql
+++ backend/sql/create-users-table.sql
@@ -16,3 +16,10 @@ CREATE TABLE users (
 
 INSERT INTO users VALUES ('admin', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Bill','Nye','admin');
 INSERT INTO users VALUES ('user', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','John','Doe','user');
+INSERT INTO users VALUES ('lhershberger', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Logan','Hershberger','admin');
+INSERT INTO users VALUES ('nbaqibillah', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Nusaiba','Baqibillah','admin');
+INSERT INTO users VALUES ('aszelestey', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Alex','Szelestey','admin');
+INSERT INTO users VALUES ('zmarvin', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Zac','Marvin','admin');
+INSERT INTO users VALUES ('kelinkowski', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Keith','Elinkowski','admin');
+INSERT INTO users VALUES ('emusk', '$2b$10$hf2WwnBkhwjXNwturHMPTuasjCipC3R3GbB9ERtkSCmMRDy.OcpZe','Elon','Musk','user');
+
