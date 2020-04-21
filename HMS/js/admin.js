1d9f4add97652671b69af24756341f3c36b9e34d
loganhersh <loganhersh@hotmail.com>
2020-04-21 12:24:06 -0400

    add dashboard refresh function

--- HMS/js/admin.js
+++ HMS/js/admin.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+  refresh();
+});
+
+function refresh() {
   getDbStatus();
   getUsersStatus();
-});
+}
 
 // Gets current status of mysql database
 function getDbStatus() {
@@ -10,29 +14,38 @@ function getDbStatus() {
   dbStatus.css('min-width',dbStatus.width());
   dbStatus.text('');
 
+  $('#refresh-icon').addClass('fa-spin');
   sendGetWithCreds(url).done( function(data, status) {
     dbStatus.text(data.state);
     var color = (data.state === 'Connected') ? '#05b600' : '#D00000';
     dbStatus.css('color',color);
+    $('#refresh-icon').removeClass('fa-spin');
   })
   .fail(function(data, status) {
     dbStatus.text('Error');
     dbStatus.css('color','#D00000')
+    $('#refresh-icon').removeClass('fa-spin');
   });
 }
 
 // Gets total users, num admins, and num users
 function getUsersStatus() {
   var url = baseApiUrl + '/users/status';
+  var usersTotal = $('#users-total');
+  var usersAdmins = $('#users-admins');
+  var usersUsers = $('#users-users');
+
   sendGetWithCreds(url).done(function(data, status) {
-    $('#users-total').text(data.total);
-    $('#users-admins').text(data.admins);
-    $('#users-users').text(data.users);
+    $('.users-data').find('span').removeClass('status-error');
+    usersTotal.text(data.total);
+    usersAdmins.text(data.admins);
+    usersUsers.text(data.users);
   })
   .fail(function(data, status) {
-    $('#user-total').text('-');
-    $('#users-admins').text('-');
-    $('#users-users').text('-');
+    $('.users-data').find('span').addClass('status-error');
+    usersTotal.text('-');
+    usersAdmins.text('-');
+    usersUsers.text('-');
   });
 }
 
