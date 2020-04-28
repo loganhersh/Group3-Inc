64f15d4e147e83b49a26b923dad8629f0952d839
loganhersh <47787402+loganhersh@users.noreply.github.com>
2020-04-28 19:06:23 -0400

    add GET reservation endpoints and functions

--- backend/routes/reservation.route.js
+++ backend/routes/reservation.route.js
@@ -4,4 +4,46 @@ const reservationController = require('../controllers/reservation.controller');
 
 router.post('/', reservationController.createReservation);
 
+/**
+ * @api {get} /reservation/id/:id
+ * @apiName Get Reservation By ID
+ * @apiPermission User
+ * @apiGroup reservation
+ *
+ * @apiParam {String} [id] Reservation ID
+ *
+ * @apiSuccess (200) {Array} [] Array of reservations in JSON format
+ * @apiFailure (404) {String} [error] Reservation not found error
+ * @apiFailure (400) {String} [error] Generic error message
+ */
+router.get('/id/:id', reservationController.getById);
+
+/**
+ * @api {get} /reservation/name/:name
+ * @apiName Get Reservation By Guest Last Name
+ * @apiPermission User
+ * @apiGroup reservation
+ *
+ * @apiParam {String} [name] Guest last name
+ *
+ * @apiSuccess (200) {Array} [] Array of reservations in JSON format
+ * @apiFailure (404) {String} [error] Reservation not found error
+ * @apiFailure (400) {String} [error] Generic error message
+ */
+router.get('/name/:name', reservationController.getByName);
+
+/**
+ * @api {get} /reservation/room/:room
+ * @apiName Get Reservation By Room Number
+ * @apiPermission User
+ * @apiGroup reservation
+ *
+ * @apiParam {Integer} [room] Reservation Room Number
+ *
+ * @apiSuccess (200) {Array} [] Array of reservations in JSON format
+ * @apiFailure (404) {String} [error] Reservation not found error
+ * @apiFailure (400) {String} [error] Generic error message
+ */
+router.get('/room/:room', reservationController.getByRoom);
+
 module.exports = router;
--- backend/services/reservation.service.js
+++ backend/services/reservation.service.js
@@ -1,5 +1,3 @@
-const addDays = require('date-fns/addDays');
-const lightFormat = require('date-fns/lightFormat');
 const db = require('../db/db');
 
 
@@ -7,18 +5,67 @@ const reservationTable = "RESERVATION";
 
 
 module.exports = {
-
+  getReservationByName,
+  getReservationById,
+  getReservationByRoom
 }
 
-// Probably unneeded
-// function getReservationsInNext365(date) {
-//   var start = date;
-//   var end = addDays(date, 365);
-//
-//   var query = "SELECT (roomtype_id, check_in_date, num_days) FROM RESERVATION "
-//       + "WHERE (check_in_date > ?) AND (check_in_date < ?)";
-//   var values = [lightFormat(start, 'yyyy-MM-dd'), lightFormat(end, 'yyyy-MM-dd')];
-// }
+const reservationSelectQuery = "SELECT r.reservation_id AS 'ReservationID', r.room_id AS 'RoomNumber'"
+    + ", r.roomtype_id AS 'RoomType', DATE_FORMAT(r.check_in_date, '%Y-%m-%e') AS 'CheckIn'"
+    + ", DATE_FORMAT(r.check_out_date, '%Y-%m-%e') AS 'CheckOut'"
+    + ", CONCAT(g.guest_lastname, ', ', g.guest_firstname) AS 'FullName'"
+    + ", g.guest_email AS 'Email', g.guest_phone AS 'Phone', g.guest_street AS 'Street'"
+    + ", g.guest_city AS 'City', g.guest_state AS 'State', g.guest_zip AS 'Zip' "
+    + "FROM reservation AS r "
+    + "INNER JOIN guest AS g "
+    + "ON r.guest_id=g.guest_id "
+    + "WHERE ??.?? = ?";
+
+
+function getReservationByName(name) {
+  const values = ['g', 'guest_lastname', name];
+
+  return new Promise((resolve, reject) => {
+    db.query(reservationSelectQuery, values, (error, results) => {
+      if(error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    })
+  });
+}
+
+
+function getReservationById(id) {
+  const values = ['r', 'reservation_id', id];
+
+  return new Promise((resolve, reject) => {
+    db.query(reservationSelectQuery, values, (error, results) => {
+      if(error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    })
+  });
+}
+
+
+function getReservationByRoom(room) {
+  const values = ['r', 'room_id', room];
+
+  return new Promise((resolve, reject) => {
+    db.query(reservationSelectQuery, values, (error, results) => {
+      if(error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    })
+  });
+}
+
 
 
 
