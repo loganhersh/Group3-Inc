7ab27d9372c06bfa0cd80877528e9d85c6737371
Zachary Marvin <zachary.marvin@live.com>
2020-04-28 11:44:10 -0500

    Adding lookup functionality

---
+++ HMS/js/lookup.js
@@ -0,0 +1,14 @@
+function searchByGuest(){
+    console.log("SEARCHING BY GUEST");
+    var searchedFName = document.getElementById("fname").value;
+    var searchedLName = document.getElementById("lname").value;
+    console.log("Searched FName is: ", searchedFName, ", and searched LName is: ", searchedLName);
+    // NEED TO LOOK UP RESERVATIONS BY FIRST AND LAST NAME
+}
+
+function searchByRoom(){
+    console.log("SEARCHING BY ROOM");
+    var searchedRoom = document.getElementById("rooms").value;
+    console.log("Searched Rooms is: ", searchedRoom);
+    // NEED TO LOOK UP RESERVATIONS BY ROOM NUMBER
+}
